a3b0d28d762b3f2016f5722bc199828c18aa2059
Brandon Yee <139765638+brandonyee-cs@users.noreply.github.com>
2025-05-22 16:34:31 -0400

    Update train.py

--- src/train.py
+++ src/train.py
@@ -364,7 +364,7 @@ def parse_args():
     # Training arguments
     parser.add_argument("--batch_size", type=int, default=32,
                         help="Batch size")
-    parser.add_argument("--num_epochs", type=int, default=100,
+    parser.add_argument("--num_epochs", type=int, default=10,
                         help="Number of epochs")
     parser.add_argument("--learning_rate", type=float, default=1e-4,
                         help="Learning rate")
