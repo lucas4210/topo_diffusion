4a937de12bbb6e024370529c5513b0a5aafbd779
Brandon Yee <brandonyee.nyc@gmail.com>
2025-05-22 08:39:20 -0400

    stuff

--- src/train.py
+++ src/train.py
@@ -17,8 +17,8 @@ import argparse
 from datetime import datetime
 
 # Import project modules
-from data import CrystalGraphDataset, CrystalGraphConverter, CrystalGraphCollator
-from model import CrystalGraphDiffusionModel
+from src.data import CrystalGraphDataset, CrystalGraphConverter, CrystalGraphCollator
+from src.model import CrystalGraphDiffusionModel
 
 # Configure logging
 logging.basicConfig(
